370dd0ce6cd143131ecc889ce89260f8694932ea
mjmammoth <awsumtoast@gmail.com>
2024-02-24 17:15:01 +0200

    constants

---
+++ constants.js
@@ -0,0 +1,5 @@
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const LIBSQL_URL = process.env.LIBSQL_URL;
+const LIBSQL_AUTH_TOKEN = process.env.LIBSQL_AUTH_TOKEN;
+
+export { OPENAI_API_KEY, LIBSQL_URL, LIBSQL_AUTH_TOKEN };
