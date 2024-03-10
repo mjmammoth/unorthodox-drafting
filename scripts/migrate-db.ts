637e8bec9859bd70937d39fba4d2155d27839311
Mark Marryatt <awsumtoast@gmail.com>
2024-03-10 18:30:11 +0200

    Handle possible null
    Signed-off-by: Mark Marryatt <awsumtoast@gmail.com>

--- scripts/migrate-db.ts
+++ scripts/migrate-db.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config';
 import { migrate } from 'drizzle-orm/libsql/migrator';
-import { db, client } from '@/lib/DB';
+import { db, libSqlClient } from '@/lib/DB';
 
 async function main() {
   console.log('Migrating database...');
   await migrate(db, { migrationsFolder: './migrations' });
-  client.close();
+  libSqlClient.close();
   console.log('Migrations complete.');
 }
 
--- src/app/api/heroes/route.ts
+++ src/app/api/heroes/route.ts
@@ -5,15 +5,13 @@ import { heroes } from '@/models';
 export async function GET(request: Request) {
   console.log('GET /api/heroes');
   console.log('request', request);
-  // const result = await db.select().from(heroes).values();
-  console.log('----------');
   const result = await statelessClient.execute({
     sql : "SELECT rowid, distance FROM vss_colours WHERE vss_search(vector, '[0.000009, 0.000001, 0.9999123]') LIMIT 15;",
     args: [],
   });
   const serialized = JSON.stringify(result.rows.map((row) => ({
     ...row,
-    rowid: row.rowid.toString(),
+    rowid: row.rowid !== null ? row.rowid.toString() : "failed_parsing",
   })));
   return Response.json(
     JSON.parse(serialized),
